2f5f23ac4c0f017cca35b69f686aab1eb6528ef3
Mike Kroutikov <mkroutikov@innodata.com>
2020-04-24 17:56:03 -0400

    use icons in the toolbar

--- ui/view/toolbar.py
+++ ui/view/toolbar.py
@@ -1,6 +1,7 @@
 from ui import qtx
 from ..event_bus import Signal
 from ..strip_args import strip_args
+from ..res import res
 
 
 class Toolbar(qtx.QToolBar):
@@ -21,19 +22,24 @@ class Toolbar(qtx.QToolBar):
 
         self._actions = []
 
-        def action(text, shortcut, emitter):
-            a = qtx.QAction(text)
-            a.triggered.connect(strip_args(emitter))
+        def action(icon=None, shortcut=None, emitter=None, text=None):
+            a = qtx.QAction()
+            if text is not None:
+                a.setText(text)
+            if emitter is not None:
+                a.triggered.connect(strip_args(emitter))
             if shortcut is not None:
                 a.setShortcut(shortcut)
+            if icon is not None:
+                a.setIcon(qtx.QIcon(icon))
             self.addAction(a)
             self._actions.append(a)
 
-        action('<', 'PgUp', self.prevPage.emit)
-        action('>', 'PgDown', self.nextPage.emit)
+        action(res('icons/sharp_arrow_back_black_48dp.png'), 'PgUp', self.prevPage.emit)
+        action(res('icons/sharp_arrow_forward_black_48dp.png'), 'PgDown', self.nextPage.emit)
 
-        action('-', 'Ctrl+-', self.scaleDown.emit)
-        action('+', 'Ctrl++', self.scaleUp.emit)
+        action(res('icons/sharp_zoom_out_black_48dp.png'), 'Ctrl+-', self.scaleDown.emit)
+        action(res('icons/sharp_zoom_in_black_48dp.png'), 'Ctrl++', self.scaleUp.emit)
 
-        action('/', 'Ctrl+/', self.rotateRight.emit)
-        action('\\', 'Ctrl+Shift+/', self.rotateLeft.emit)
+        action(res('icons/sharp_rotate_left_black_48dp.png'), 'Ctrl+Shift+/', self.rotateLeft.emit)
+        action(res('icons/sharp_rotate_right_black_48dp.png'), 'Ctrl+/', self.rotateRight.emit)
